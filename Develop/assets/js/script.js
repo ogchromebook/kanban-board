b97d56976ff75cc4bd33d815a371f32825cbb6c7
Sam Kachergius <skachergius@gmail.com>
2024-07-23 13:41:01 -0500

    added js functions for task ID generation and task card creation

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,12 +4,20 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    localStorage.setItem("nextId", nextId + 1);
+    return nextId++;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+    return `
+    <div class="task-card" data-id="${task.id}">
+        <h3>${task.title}</h3>
+        <p>${task.description}</p>
+        <p>Due: ${task.dueDate}</p>
+        <button class="delete-task">Delete</button>
+    </div>
+`;
 }
 
 // Todo: create a function to render the task list and make cards draggable
